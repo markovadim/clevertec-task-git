d71610ac08bb344f7e1c765cab1c93c22eb19daf
markov vadim <markovadim@gmail.com>
2023-02-05 22:28:41 +0300

    develop: added java counter

---
+++ Counter.java
@@ -0,0 +1,9 @@
+public class CounterVariableExample1{  
+    public static void main(String[] args) {  
+        int counter = 0;  
+        for(int i=0; i<5; i++){    
+            counter = counter + 1;   
+            System.out.println(counter);  
+        }  
+    }  
+}  
